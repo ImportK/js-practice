4cff0dd5fdb9eb5000e9ca68b8e1f7c26c404833
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-09-07 10:43:53 +0800

    Completed Variables Section

---
+++ app.js
@@ -0,0 +1,85 @@
+// Variables
+let color = "Green"
+console.log(color)
+
+let fruit = "apple"
+console.log(fruit)
+
+let flower = "Dandelions";
+console.log(flower);
+
+let name = "Sara"
+let age = 7
+console.log("Name:" , name)
+console.log("Age:" , age , "years old")
+
+
+// Numbers
+const num1 = 5;
+const num2 = 3;
+const modulus = num1 % num2;
+const exponent = num1 ** num2;
+console.log("Modulus: " + modulus);
+console.log("Exponentiation: " + exponent);
+
+const favNum1 = 18;
+const favNum2 = 4;
+const sum = favNum1 + favNum2;
+const difference = favNum1 - favNum2;
+const product = favNum1 * favNum2;
+const quotient  = favNum1 / favNum2;
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product)
+console.log("Quotient: " + quotient)
+
+
+// Booleans
+let isOffline = false;
+let isAsleep = true;
+console.log(isOffline);
+console.log(isAsleep);
+
+let isEmpty = Boolean('');
+let isNotEmpty = Boolean("JavaScript");
+console.log(isEmpty);
+console.log(isNotEmpty);
+
+let SuperTyphoon = true;
+let inChina  = false;
+console.log("Does typhoon Gener a super typhoon?" , SuperTyphoon);
+console.log("Does Monkey Pox originate in China?" , inChina);
+
+let a = 4;
+let b = 8;
+let isGreater = a < b;
+let isEqual = a == b;
+console.log(isGreater);
+console.log(isEqual);
+
+
+// Comparison Operators
+let x = 12
+let y = 34
+
+let isXGreaterThanY = x > y;
+let isXLessThanY = x < y;
+let isXEqualToY = x === y;
+let isXNotEqualToY = x !== y;
+
+console.log(" Is X greater than Y?" , isXGreaterThanY);
+console.log(" Is X less than Y?" , isXLessThanY); 
+console.log(" Is X equal to Y?" , isXEqualToY);
+console.log(" Is X not equal to Y?" , isXNotEqualToY);
+
+// Strings
+let myName = "Kaye Janine";
+let feedback = ("Well done, " + myName + "!");
+let upperCasemyName = myName.toUpperCase();
+let lowerCasemyName = myName.toLowerCase();
+let myNameLength = myName.length;
+
+console.log(feedback);
+console.log("Uppercase Name " + upperCasemyName);
+console.log("Lowercase Name: " + lowerCasemyName);
+console.log("Name Length: " + myNameLength);
